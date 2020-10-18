a27d8bcea3ea25b80179d0dc2d6966c55d62b481
Steve <4391026+torvista@users.noreply.github.com>
2020-10-18 09:41:01 +0200

    sql texts

--- sql/updates/update_1_15_0.sql
+++ sql/updates/update_1_15_0.sql
@@ -1,14 +1,14 @@
 #version
 UPDATE configuration SET configuration_value = '1.15.0' WHERE configuration_key = 'GOOGLE_PRODUCTS_VERSION' LIMIT 1;
 
-#Add option Products Options Stock to 3rd party attribute-stock plugins
+#Add option Products Options Stock to 3rd party attribute-stock plugins dropdown
 UPDATE configuration SET 
 configuration_title = 'Attribute-Stock Plugin',
 configuration_description = 'Select the third party plugin used for managing attribute/variant stocks or leave as \'none\'.',
 set_function = 'zen_cfg_select_drop_down(array(array(\'id\' => \'none\', \'text\' => \'none\'), array(\'id\' => \'stockbyattributes\', \'text\' => \'Stock By Attributes\'), array(\'id\' => \'numinixproductvariants\', \'text\' => \'Numinix Product Variants\'), array(\'id\' => \'posm\', \'text\' => \'Products Options Stock Manager\')),'
 WHERE configuration_key = 'GOOGLE_PRODUCTS_SWITCH_STOCK_PLUGIN' LIMIT 1;
 
-#Add option CEON Advanced Shipper to shipping methods
+#Add option CEON Advanced Shipper to shipping methods radio buttons
 UPDATE configuration SET 
 configuration_description = 'Select the name of the shipping module used in your store (as shown in Modules->Shipping), or leave as \'none\'.',
 set_function = 'zen_cfg_select_option(array(\'none\', \'flat\', \'freeshipper\', \'item\', \'perweightunit\', \'table\', \'zones\', \'advshipper\', \'freerules\', \'percategory\', \'zonetable\'),'
